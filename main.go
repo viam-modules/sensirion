cae04da735ceccbeca73d2afa1778737bb821f3e
martha-johnston <martha.johnston@viam.com>
2024-10-31 16:33:00 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+// package main is a module with raspberry pi board component.
+package main
+
+import (
+	"context"
+
+	"sensirion/sht3xd"
+
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
+	"go.viam.com/utils"
+)
+
+func main() {
+	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("NAME"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) error {
+	module, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	if err = module.AddModelFromRegistry(ctx, sensor.API, sht3xd.Model); err != nil {
+		return err
+	}
+
+	err = module.Start(ctx)
+	defer module.Close(ctx)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	return nil
+}
---
+++ sht3xd/sht3xd_nonlinux.go
@@ -0,0 +1,2 @@
+// Package sht3xd is only supported on Linux machines.
+package sht3xd
